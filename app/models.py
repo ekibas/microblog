bd2a1d8fcc0f2d10d81b822db3d72b3662e8299c
ekibas <makandreev@korusconsulting.ru>
2018-12-09 16:12:39 +0300

    #3 add sign up

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
-from app import db
+from app import db, login
 from datetime import datetime
 from werkzeug import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -15,6 +16,10 @@ class User(db.Model):
         return check_password_hash(self.password_hash, password)
     def __repr__(self):
         return '<User {}>'.format(self.username) 
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
